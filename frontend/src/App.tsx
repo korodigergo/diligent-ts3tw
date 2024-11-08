ca38099ab73df8c6391dd1ce1140e64f02986b13
korodigergo <127103460+korodigergo@users.noreply.github.com>
2024-11-08 17:57:13 +0100

    install react router dom and create some components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,27 @@
-function App() {
+import { NavLink, Outlet } from "react-router-dom";
 
+function App() {
   return (
     <>
+      <nav id="sidenav">
+        <NavLink to="/" className="nav-link">
+          Home
+        </NavLink>
+        <NavLink to="/users" className="nav-link">
+          Users
+        </NavLink>
+        <NavLink to="/posts" className="nav-link">
+          Posts
+        </NavLink>
+      </nav>
+
+      <div className="root-layout">
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,36 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import PostsPage from './pages/PostsPage.tsx';
+import UsersPage from './pages/UsersPage.tsx';
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    children: [
+      {
+        path: "/posts",
+        element: <PostsPage/>,
+      },
+      {
+        path: "/users",
+        element: <UsersPage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>,
 )
