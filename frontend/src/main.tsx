1141d0745f5c5e3f72d0fc1cb87531b3f9db8f70
zsolt270 <zsolt270000@gmail.com>
2024-11-14 00:29:45 +0100

    deleted an unecessarry line in the main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,37 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App.tsx";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import PostsPage from './pages/PostsPage.tsx';
-import UsersPage from './pages/UsersPage.tsx';
+import PostsPage from "./pages/PostsPage.tsx";
+import UsersPage from "./pages/UsersPage.tsx";
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    children: [
-      {
-        path: "/posts",
-        element: <PostsPage/>,
-      },
-      {
-        path: "/users",
-        element: <UsersPage />,
-      },
-    ],
-  },
+	{
+		path: "/",
+		element: <App />,
+	},
+	{
+		children: [
+			{
+				path: "/posts",
+				element: <PostsPage />,
+			},
+			{
+				path: "/users",
+				element: <UsersPage />,
+			},
+		],
+	},
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	</StrictMode>
+);
